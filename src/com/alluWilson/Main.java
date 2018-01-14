fcc06eb5c8a78febd7a6e602627e312f8514fc15
Allan Wilson <allan@allanwilson.net>
2018-01-15 00:38:08 +0200

    Sunday night

--- src/com/alluWilson/Main.java
+++ src/com/alluWilson/Main.java
@@ -3,6 +3,14 @@ package com.alluWilson;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        groceryList AllansList = new groceryList();
+        AllansList.addGroceryItem("Milk");
+        AllansList.addGroceryItem("Ham");
+        AllansList.printGroceryList();
+        AllansList.modifyGroceryItem(1,"Eggs");
+        AllansList.printGroceryList();
+        AllansList.removeGroceryItem(1);
+        AllansList.printGroceryList();
     }
+
 }
--- src/com/alluWilson/MobilePhone.java
+++
@@ -1,4 +0,0 @@
-package com.alluWilson;
-
-public class MobilePhone {
-}
